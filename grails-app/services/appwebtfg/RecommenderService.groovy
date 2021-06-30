13a73e39536f1cccf9d6952134b0b3c1f8409682
dimonra13 <diegomontoto4@hotmail.com>
2021-06-30 20:10:51 +0200

    User profile for API queries updated

--- grails-app/services/appwebtfg/RecommenderService.groovy
+++ grails-app/services/appwebtfg/RecommenderService.groovy
@@ -328,7 +328,10 @@ class RecommenderService {
             ]
         else {
             List<Course> courses = user?.lists?.courses?.flatten()
-            String description = " " + user?.interests?.join(",")+courses?.collect{course -> course.title}?.join(",")
+            String description = " " + user?.interests?.join(",")+
+                    (user?.basicSkills) ? ","+user?.basicSkills?.collect{it.name}?.join(",") : "" +
+                    (user?.mediumSkills) ? ","+user?.mediumSkills?.collect{it.name}?.join(",") : "" +
+                    (user?.expertSkills) ? ","+user?.expertSkills?.collect{it.name}?.join(",") : ""
             String difficulty = calculateAvgDifficulty(courses,user)
             int free = calculateIsFree(courses)
             float rating = calculateAvgRating(courses)
